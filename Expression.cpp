56ebde5018786144bcd15b489054759f265f3948
Boooobbbyy <48676640+Boooobbbyy@users.noreply.github.com>
2019-04-24 22:57:30 +0700

    Update Expression.cpp

--- Expression.cpp
+++ Expression.cpp
@@ -4,3 +4,54 @@
    1817051021 Arafia Isnayu Akaf
    1817051049 Bobby Malela
 */
+#include <iostream>
+using namespace std;
+//Yang Ini Berfungsi Untuk input Charakter pada infix
+int N(char input){
+	if(input == '^')
+		return 3;
+	else if(input == '*' || input == '/')
+		return 2;
+	else if(input == '+' || input == '-')
+		return 1;
+	else if(input == '(' || input == ')')
+		return 0;
+	else
+		return -1;
+}
+// ini stacknya di batesin 
+struct stack{
+	int STACK[100000];
+	char Stack[100000];
+	int top;
+};
+//ini kelas untuk konversi
+class konversi{
+private:
+	stack list;
+
+public:
+	void init(){
+		list.top = -1;
+	}
+
+	void push(char input){
+		list.top++;
+		list.STACK[list.top] = input;
+	}
+
+	void pop(){
+		list.top--;
+	}
+
+	char Top(){
+		return list.STACK[list.top];
+	}
+
+	bool isEmpty(){
+		if(list.top <= -1)
+			return true;
+		else
+			return false;
+	}
+}; konversi SC;
